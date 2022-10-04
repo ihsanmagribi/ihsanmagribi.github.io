ac3c88217a58a26108389708e1d6bb3c8df280db
Ihsan Magribi <ihsanmahgribi@gmail.com>
2022-10-05 01:13:45 +0800

    posttes3

--- posttes3/script.js
+++ posttes3/script.js
@@ -1,10 +1,6 @@
 var masuk = confirm("Klik OK untuk masuk dan CANCEL untuk kembali")
 if(masuk == true){
     console.log("bershasil masuk");
-    var a = "Var A";
-    let b = "Let B";
-    const c = "Const C";
-
 }
 
 else{
